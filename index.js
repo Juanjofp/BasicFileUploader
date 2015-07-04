b85ea84c1350058b9e46e14fc5a294274e89133c
Juanjo <juanjo@juanjofp.com>
2015-07-04 18:46:39 +0200

    CSS iage rounded

--- index.js
+++ index.js
@@ -18,8 +18,20 @@
         eleUpload2 = document.getElementById('upload2'),
         eleProgress2 = document.getElementById('progress2'),
 
+        eleFile11 = document.getElementById('file11'),
+        img1 = document.getElementById('img1'),
+        eleFile21 = document.getElementById('file21'),
+        img2 = document.getElementById('img2'),
+        eleFile31 = document.getElementById('file31'),
+        img3 = document.getElementById('img3'),
+        eleFile41 = document.getElementById('file41'),
+        img4 = document.getElementById('img4'),
+        eleUpload3 = document.getElementById('upload3'),
+        eleProgress3 = document.getElementById('progress3'),
+
         uploader = FUploader(),
-        uploader2 = MUploader();
+        uploader2 = MUploader(),
+        uploader3 = SMUploader();
 
     uploader.init(eleFile, preview, eleProgress);
 
@@ -27,6 +39,11 @@
         {file: eleFile2, preview: preview2},
         {file: eleFile3, preview: preview3},
         {file: eleFile4, preview: preview4}], eleProgress2);
+
+    uploader3.init([{file: eleFile11, preview: img1},
+        {file: eleFile21, preview: img2},
+        {file: eleFile31, preview: img3},
+        {file: eleFile41, preview: img4}], eleProgress3);
 
     // URL file uploader
     var url = 'http://localhost:3123/api/v1/files/';
@@ -61,6 +78,23 @@
             }, 
             function rejected(err) {
                 console.log('Promise Multi Error', err);
+            }
+        );
+    };
+
+    eleUpload3.onclick= function onClick(evt) {
+        evt.preventDefault();
+        var data = {};//Object.create(null);
+        data.valor1 = 'valor 1';
+        data.valor2 = 'valor 2';
+        data.valor3 = 'valor 3';
+        data.valor4 = 'valor 4';
+        uploader3.upload(url, data).then(
+            function fulfilled(data){
+                console.log('Promise Single Multi OK', JSON.parse(data));
+            }, 
+            function rejected(err) {
+                console.log('Promise Single Multi Error', err);
             }
         );
     };
---
+++ singlemultiuploader.js
@@ -0,0 +1,156 @@
+var SMUploader = (function IIFE() {
+    'use strict';
+
+    function createImagePreview(file, imgTag) {
+        var reader = new FileReader();
+
+        reader.onload = function loadImage(evt) {
+            //console.log('onload file', evt.target.result);
+            imgTag.src = evt.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+
+    function init(dataElements, progress) {
+        this.eleFiles = dataElements;
+        this.progress = progress;
+        this.progress.value = 0;
+        for(var i = 0; i < this.eleFiles.length; ++i) {
+            var inputFile = this.eleFiles[i].file;
+            inputFile.onchange = onFilesSelecteds.bind(this);
+            console.log(inputFile);
+        }
+    }
+
+    function listFilesSelected(inputTarget) {
+        if(this.eleFiles && this.eleFiles.length > 0) {
+            for(var j = 0; j < this.eleFiles.length; j++) {
+
+                console.log('Matchibg', inputTarget, this.eleFiles[j]);
+                if(inputTarget !== this.eleFiles[j].file) {
+                    continue;
+                }
+
+                var inputFile = this.eleFiles[j].file;
+                var preview = this.eleFiles[j].preview;
+                console.log('show', inputFile, preview);
+                if(inputFile && inputFile.files && preview) {
+                    var file = inputFile.files[0];
+                    createImagePreview(file, preview);
+                }
+            }
+        }
+    }
+
+    function uploadData(url, data, resolve, reject) {
+        var formData = new FormData();
+        var fire = false;
+
+        if(this.eleFiles && this.eleFiles.length > 0) {
+            for(var j = 0; j < this.eleFiles.length; j++) {
+                var inputFile = this.eleFiles[j].file;
+                if(inputFile && inputFile.files) {
+                    var file = inputFile.files[0];
+                    if(!file.type.match('image.*')) {
+                        return;
+                    }
+                    //console.log('Encapsulando file: ', inputFile.name, file, file.name);
+                    formData.append(inputFile.name, file, file.name);
+                    fire = true;
+                }
+            }
+        }
+
+        if(data) {
+            for (var key in data) {
+                if (data.hasOwnProperty(key)) {
+                    //console.log('Encapsulando: ', key, data[key]);
+                    formData.append(key, data[key]);
+                    fire = true;
+                }
+            }
+        }
+
+        if(fire) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', url, true);
+
+            xhr.upload.addEventListener('progress', onXHRProgress.bind(this), false);
+            //xhr.upload.addEventListener('load', onXHRLoad.bind(this, resolve), false);
+            xhr.upload.addEventListener('error', onXHRError.bind(this, reject), false);
+            xhr.upload.addEventListener('abort', onXHRAbort.bind(this, reject), false);
+            xhr.addEventListener('load', onXHRLoad.bind(this, resolve), false);
+            xhr.addEventListener('error', onXHRError.bind(this, reject), false);
+            xhr.addEventListener('abort', onXHRAbort.bind(this, reject), false);
+
+            try {
+                xhr.send(formData);
+            }
+            catch(err) {
+                reject(err);
+            }
+        }
+        else {
+            reject(new Error('No data to upload'));
+        }
+    }
+
+    function upload(url, data) {
+        var fn = uploadData.bind(this);
+        return new Promise(function executor(resolve, reject) {
+            fn(url, data, resolve, reject);
+        });
+    }
+
+    function onXHRLoad(resolve, evt) {
+        //console.log('Files loaded!', resolve, evt);
+        if(this.progress) {
+            this.progress.value = 1;
+        }
+        resolve(evt.target.response);
+    }
+
+    function onXHRError(reject, evt) {
+        //console.log('Error on upload', reject, evt);
+        reject(evt);
+    }
+
+    function onXHRAbort(reject, evt) {
+        //console.log('Abort Uploader', reject, evt);
+        reject(evt);
+    }
+
+    function onXHRProgress(evt) {
+        if (evt.lengthComputable) {
+            var percentComplete = evt.loaded / evt.total;
+            //console.log('Current Progress: ', percentComplete);
+            if(this.progress) {
+                this.progress.value = percentComplete;
+            }
+        } else {
+            // Unable to compute progress information since the total size is unknown
+            //console.log('Uncomputable progress');
+            if(this.progress) {
+                this.progress.value = '-1';
+            }
+        }
+    }
+
+    function onFilesSelecteds(evt) {
+        //console.log('onclick', evt);
+        evt.preventDefault();
+        //console.log('Files changed');
+        listFilesSelected.call(this, evt.target);
+    }
+
+    var Uploader = {
+        init: init,
+        list: listFilesSelected,
+        upload: upload
+    };
+
+    return function createUploader() {
+        var uploader = Object.create(Uploader);
+        return uploader;
+    };
+})();
